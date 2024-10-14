4035c4ff630fd6ff01a33e338a186c8b54e5dfeb
Tim <tim103@pm.me>
2024-10-15 09:41:44 +1100

    Added metadatas and images like icons/logo/og image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,19 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       htmlAttrs: { lang: 'en' },
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/img/logo/favicon.png' }],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/img/icon.svg' },
+        { rel: 'apple-touch-icon', href: '/img/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/manifest.webmanifest' }
+      ],
       meta: [
-        { hid: 'robots', name: 'robots', content: 'index, follow' },
-        { hid: 'googlebot', name: 'googlebot', content: 'index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1' },
-        { hid: 'bingbot', name: 'bingbot', content: 'index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1' },
-        { hid: 'robots', property: 'og:site_name', content: 'ColorMagic' }
+        { name: 'robots', content: 'index, follow' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'googlebot', content: 'index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1' },
+        { name: 'bingbot', content: 'index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1' },
+        { property: 'og:site_name', content: 'ColorMagic' },
+        { property: 'og:image', content: '/img/og.png' }
       ]
     }
   },
