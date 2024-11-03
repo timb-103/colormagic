9bd9065189ce2a65ec548630a4f23eeddee5fe67
Tim <tim103@pm.me>
2024-11-03 15:49:27 +1100

    Fixed issue with images.

--- layers/og/server/api/og/thumbnail/index.get.ts
+++ layers/og/server/api/og/thumbnail/index.get.ts
@@ -1,4 +1,4 @@
-export default defineCachedEventHandler(async (event) => {
+export default defineEventHandler(async (event) => {
   const query = await modules.og.validation.getThumbnailInputQuery(event);
 
   const response = await modules.og.service.generateSquareThumbnail(
@@ -8,4 +8,4 @@ export default defineCachedEventHandler(async (event) => {
   setResponseHeader(event, 'Content-Type', 'image/png');
 
   return response;
-}, { maxAge: 86400 });
+});
