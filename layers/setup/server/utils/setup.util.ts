c25405cf52d1db4bc631caf9f813b99e79be3f2b
Tim <tim103@pm.me>
2024-11-07 08:44:08 +1100

    TS fix

--- layers/setup/server/utils/setup.util.ts
+++ layers/setup/server/utils/setup.util.ts
@@ -1,7 +1,6 @@
 import type { Logger } from 'pino';
 import { getAIModule, type AIModule } from '~/layers/ai/server/ai.module';
 import { getAuthModule, type AuthModule } from '~/layers/auth/server/auth.module';
-import { getFeedbackModule, type FeedbackModule } from '~/layers/feedback/server/feedback.module';
 import { getGoogleModule, type GoogleModule } from '~/layers/google/server/google.module';
 import { getLoggerModule } from '~/layers/log/server/logger.module';
 import { getMongoModule } from '~/layers/mongo/server/mongo.module';
@@ -15,7 +14,6 @@ interface Modules {
   logger: Logger
   palette: PaletteModule
   og: OgModule
-  feedback: FeedbackModule
   auth: AuthModule
   user: UserModule
   google: GoogleModule
@@ -39,7 +37,6 @@ export async function setup(): Promise<void> {
     const ai = getAIModule(logger, openai.service);
     const palette = getPaletteModule(db, logger, ai.service);
     const og = getOgModule(logger, palette.service);
-    const feedback = getFeedbackModule(db, logger);
     const user = getUserModule(db, logger);
     const google = getGoogleModule(logger);
     const auth = getAuthModule(logger, user.service, google.service);
@@ -51,7 +48,6 @@ export async function setup(): Promise<void> {
       logger,
       palette,
       og,
-      feedback,
       auth,
       user,
       google
