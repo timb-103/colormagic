c68ef0c88b4facf39c1a0f6c5b0c4d2bad155564
Tim <tim103@pm.me>
2024-11-30 19:48:09 +1100

    Added a simple about page.

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- header-->
+    <div class="mb-8">
+      <!-- title -->
+      <h1>
+        About
+      </h1>
+
+      <!-- description-->
+      <p class="text-xl font-medium mb-4 max-w-xl">
+        ColorMagic is the ultimate color palette generator that utilizes AI to create beatiful color palettes ready for your designs.
+      </p>
+
+      <!-- description-->
+      <p class="text-xl font-medium mb-4 max-w-xl">
+        Roughly 200,000 people use ColorMagic per month to create hundreds of thousands of palettes.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
