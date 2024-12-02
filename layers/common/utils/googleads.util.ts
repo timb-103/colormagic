be3912e00c6f5fb952874ac89da6539f809c5909
Tim <tim103@pm.me>
2024-12-02 17:41:35 +1100

    Fix

--- layers/common/utils/googleads.util.ts
+++ layers/common/utils/googleads.util.ts
@@ -2,7 +2,7 @@ export function refreshGoogleAds(): void {
   const els = document.getElementsByClassName('adsbygoogle');
 
   for (const el of els) {
-    if (el.getAttribute('data-vignette-loaded') === null && el.getAttribute('data-ad-status') === 'done') {
+    if (el.getAttribute('data-vignette-loaded') === null && el.getAttribute('data-adsbygoogle-status') === 'done') {
       el.removeAttribute('data-ad-status');
       el.removeAttribute('data-adsbygoogle-status');
       el.innerHTML = '';
