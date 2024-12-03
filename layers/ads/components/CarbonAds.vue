614783df619ed385914d6e5c0041f5df723bf0d7
Tim <tim103@pm.me>
2024-12-03 16:03:58 +1100

    Testing some new carbon placements.

--- layers/ads/components/CarbonAds.vue
+++ layers/ads/components/CarbonAds.vue
@@ -29,9 +29,9 @@ function init(): void {
   }
 }
 
-watch(useRoute(), () => {
-  // window?._carbonads?.refresh();
-});
+// watch(useRoute(), () => {
+//   window?._carbonads?.refresh();
+// });
 
 onMounted(() => {
   init();
