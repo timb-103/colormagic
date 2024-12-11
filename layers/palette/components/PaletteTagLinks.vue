0dac529eeceb3e3a8cbc1fc172d02b8cba76e989
Tim <tim103@pm.me>
2024-12-11 16:54:28 +1100

    Changed tag links to a list.

--- layers/palette/components/PaletteTagLinks.vue
+++ layers/palette/components/PaletteTagLinks.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="flex flex-wrap gap-2">
-    <!-- languages -->
-    <NuxtLink
+  <ul class="flex flex-wrap gap-2">
+    <li
       v-for="item in links.filter(v => v.id !== '')"
       :key="item.id"
-      :to="item.to"
-      :title="`${item.label} Color Palettes`"
-      :aria-label="`${item.label} Color Palettes`"
-      active-class="text-primary"
-      class="font-medium text-sm hover:text-primary text-gray-400"
     >
-      {{ item.label }}
-    </NuxtLink>
-  </div>
+      <NuxtLink
+        :to="item.to"
+        :title="`${item.label} Color Palettes`"
+        :aria-label="`${item.label} Color Palettes`"
+        active-class="text-primary"
+        class="font-medium text-sm hover:text-primary text-gray-400"
+      >
+        {{ item.label }}
+      </NuxtLink>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
